5a606e03ab9bab7744c44e366771fcf3496528d7
AlexAlkeev <alkeevsasha@mail.ru>
2025-11-27 14:21:42 +0300

    Delete hw09-jdbc directory

--- hw09-jdbc/src/main/java/ru/otus/necessary/sessionmanager /DataBaseOperationException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.necessary.sessionmanager;
-
-public class DataBaseOperationException extends RuntimeException {
-    public DataBaseOperationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
