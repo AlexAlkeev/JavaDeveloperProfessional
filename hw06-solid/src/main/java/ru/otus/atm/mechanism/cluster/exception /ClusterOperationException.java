25bac3b23ff118cd2eb958abe8c360e822b665b1
AlexAlkeev <alkeevsasha@mail.ru>
2025-11-27 07:37:22 +0300

    Delete hw06-solid directory

--- hw06-solid/src/main/java/ru/otus/atm/mechanism/cluster/exception /ClusterOperationException.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.atm.mechanism.cluster.exception;
-
-public class ClusterOperationException extends Exception {
-    public ClusterOperationException() { }
-
-    public ClusterOperationException(String message) { super(message); }
-
-    public ClusterOperationException(String message, Throwable cause) { super(message, cause); }
-}
