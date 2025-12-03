14d507066ffe66553b1ea9bc2a9c0eefe006451e
AlexAlkeev <alkeevsasha@mail.ru>
2025-12-03 08:42:20 +0300

    Delete hw06-solid directory

--- hw06-solid/src/main/java/ru/otus/atm/mechanism/cell/exception /CellOperationException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.atm.mechanism.cell.exception;
-
-public class CellOperationException extends Exception {
-    public CellOperationException(String message) { super(message); }
-
-    public CellOperationException(String message, Throwable cause) { super(message, cause); }
-}
