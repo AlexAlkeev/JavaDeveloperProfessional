ae34eda24500241b63dba99598ccfa836496733f
AlexAlkeev <alkeevsasha@mail.ru>
2025-12-04 08:24:37 +0300

    Delete hw12-servlets directory

--- hw12-servlets/src/main/java/ru/otus/configuration/hibernate /HibernateConfiguration.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.configuration.hibernate;
-
-import ru.otus.service.db.DBServiceClient;
-
-public interface HibernateConfiguration {
-    DBServiceClient configure();
-}
--- hw12-servlets/src/main/java/ru/otus/service/web/server /ClientWebServer.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.service.web.server;
-
-public interface ClientWebServer {
-    void start() throws Exception;
-
-    void join() throws Exception;
-
-    void stop() throws Exception;
-}
